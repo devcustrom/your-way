905d9888848dc3de71edacafc30f08b5979799f2
devcustrom <devcustrom@gmail.com>
2025-12-30 11:13:17 +0500

    Типизация

--- src/tools/fitText.ts
+++ src/tools/fitText.ts
@@ -1,11 +1,14 @@
-
-export function fitText(parent, text, options = {}) {
+export function fitText(parent: HTMLElement, text: HTMLElement, options: {
+	max?: number
+} = {}) {
 	const parentWidth = parent.getBoundingClientRect().width
     const { width, height } = text.getBoundingClientRect()
 	const newHeight = parentWidth / (width / height)
 	const style = window.getComputedStyle(text, null).getPropertyValue('font-size');
 	const currentFontSize = parseFloat(style);
 	const newFontSize = newHeight / (height / currentFontSize)
-	const { max } = options
+	const {
+		max
+	} = options
 	text.style['font-size'] = `${ max && newFontSize > max ? max : newFontSize}px`
 }
